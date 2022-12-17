b347236aa83ed07bc9ce202dfb1587287fda4183
karinaKhairullina <95102785+karinaKhairullina@users.noreply.github.com>
2022-12-17 21:50:05 +0300

    Update server.py

--- controlSecond/server.py
+++ controlSecond/server.py
@@ -1,70 +1,21 @@
 import socket
 
-LOCALHOST = "127.0.0.1"
-PORT = 5040
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.bind(('', 55000))
+socket.listen(2)
+print('Сервер работает ура победа')
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((LOCALHOST, PORT))
-server.listen(1)
-
-clientConnection, clientAddress = server.accept()
-
-msg = ''
-array = []
+server = []
 
 while True:
-    data = clientConnection.recv(1024)
-    message = data.decode()
+    conn, addr = socket.accept()
+    data = conn.recv(1024)
+    server.append(data)
+    print(server)
+    print(str(data))
+    conn.send(data.upper())
+conn.close()
 
-    if message == 'Exit':
-        print("Соединение прервано")
-        break
 
-    print("Результат")
-    result = 0
-    operation_list = message.split()
-    first_num = operation_list[0]
-    num1 = int(first_num)
 
-    if len(operation_list) == 3:
 
-        operation = operation_list[1]
-        second_num = operation_list[2]
-        number2 = int(second_num)
-
-        if operation == "+":
-            result = num1 + number2
-        elif operation == "-":
-            result = num1 - number2
-        elif operation == "/":
-            result = num1 / number2
-        elif operation == "*":
-            result = num1 * number2
-        array.append(result)
-        output = str(result)
-
-    elif len(operation_list) == 2:
-        operation = operation_list[1]
-        if operation == "+":
-            result = array[len(array) - 1] + num1
-        elif operation == "-":
-            result = array[len(array) - 1] - num1
-        elif operation == "/":
-            result = array[len(array) - 1] / num1
-        elif operation == "*":
-            result = array[len(array) - 1] * num1
-        array.append(result)
-        output = str(result)
-
-    elif len(operation_list) == 1:
-        if operation_list[0] == 'history':
-            result = ''
-            for i in array:
-                result = result + ' ' + str(i)
-            output = result
-            clientConnection.send(output.encode())
-        else:
-            output = 'Что ты такое отправил...'
-
-    clientConnection.send(output.encode())
-clientConnection.close()

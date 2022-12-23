73f3a36026a942586a623023e3608093b8f1eefc
karinaKhairullina <95102785+karinaKhairullina@users.noreply.github.com>
2022-12-23 20:35:34 +0300

    sw

---
+++ Semestrovka/funCinema/views.py
@@ -0,0 +1,40 @@
+
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+from django.views.generic.edit import CreateView
+from .models import Actor, Genre
+from .models import Serials
+
+
+class SerialsView(View):
+    def get(self, request):
+        serials = Serials.objects.all()
+        return render(request, "serials/serials_list.html", {"serials": serials})
+
+
+class SerialDetailView(View):
+    def get(self, request):
+        serials = Serials.objects.all()
+        genres = Genre.objects.all()
+        return render(request, "serials/single_serial.html", {"serials": serials, "genres": genres})
+
+
+class SingleSerial(View):
+    def get(self, request, slug):
+        serial = get_object_or_404(Serials, url=slug)
+        return render(request, "serials/detail_serial.html", {"serial": serial})
+
+
+class ActorView(View):
+    def get(self, request, id):
+        serial = get_object_or_404(Actor, id=id)
+        return render(request, "serials/actor.html", {"serial": serial})
+
+
+class AddView(CreateView):
+    model = Serials
+    template_name = 'serials/add.html'
+    fields = ['title', 'description', 'date', 'country', 'actors']
+
+
+
